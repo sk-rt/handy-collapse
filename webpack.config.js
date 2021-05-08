db4bd585b0bb927d510000da040e10e48fe52074
Ryuta Sakai <info@sakairyuta.com>
2021-05-08 11:44:21 +0900

    chore: upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
 const base = require("./webpack.config.base");
-let devMode, publicPath;
-if (process.env.NODE_ENV === "production") {
-    publicPath = "";
-    devMode = "production";
-} else {
-    publicPath = "js/";
-    devMode = "development";
-}
+const environment = process.env.NODE_ENV || "development";
+const isDevelopment = environment === "development";
+const publicPath = isDevelopment ? "js/" : "";
+
 /**
  * For Es Module
  */
 const esmOutputPath =
-    process.env.NODE_ENV === "production" ? `${__dirname}/dist/${publicPath}` : `${__dirname}/dev/${publicPath}`;
+    process.env.NODE_ENV === "production" ? `${__dirname}/dist/${publicPath}` : `${__dirname}/sample/${publicPath}`;
 const esm = {
     ...base,
     entry: {
         index: `${__dirname}/src/index.ts`
     },
-    mode: devMode,
+    mode: isDevelopment ? environment : "production",
     output: {
         path: esmOutputPath,
         publicPath: publicPath,
@@ -32,17 +28,27 @@ const esm = {
 const standAloneOutputPath =
     process.env.NODE_ENV === "production"
         ? `${__dirname}/dist/standalone/${publicPath}`
-        : `${__dirname}/dev/${publicPath}`;
+        : `${__dirname}/sample/${publicPath}`;
 const standAlone = {
     ...base,
     entry: {
         "handy-collapse": `${__dirname}/src/index-standalone.ts`
     },
-    mode: devMode,
+    mode: isDevelopment ? environment : "production",
+
     output: {
         path: standAloneOutputPath,
         publicPath: publicPath,
         filename: "[name].js"
+    },
+    devServer: {
+        contentBase: `${__dirname}/sample/`,
+        watchContentBase: true,
+        open: true,
+        host: "0.0.0.0",
+        useLocalIp: true,
+        port: 8888
     }
 };
+
 module.exports = [esm, standAlone];
